c754f49e575e3ea79fd44d4eceb337b1c12af0b0
Claudia Junko OKabe <cjokabe@uol.com.br>
2021-05-04 20:28:53 -0300

    Acrescentado desejando um bom dia!

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -8,6 +8,7 @@ namespace projeto_teste
         {
             Console.WriteLine("Olá mundo!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Tenha um bom dia!");
             Console.ReadLine();
         }
     }
